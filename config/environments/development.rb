93df28e42efba017588a54790df35c8d1db853f0
Pinelo <pinelo93@gmail.com>
2015-02-25 19:23:50 -0600

    after devise:install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.action_mailer.perform_deliveries = true 
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address                  => "smtp.gmail.com",
