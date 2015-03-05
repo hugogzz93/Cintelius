d5af511086d69a406be3ad929fd5890000d082d8
Pinelo <pinelo93@gmail.com>
2015-03-04 19:29:47 -0600

    Testing sublime-git plugins

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,9 @@ class OrdersController < ApplicationController
 	def index
 		@orders = Order.all
 	end
+
+	def new
+		@categories = Category.all
+		@order = Order.new
+	end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
 	has_many :products
-	has_many :subcategories, class_name: "Category", foreign_key: "category_id"
+	has_many :subcategories, class_name: "Category", 
+							  foreign_key: "supercategory_id"
+
 	belongs_to :supercategory, class_name: "Category"
 
 	validates :name, uniqueness: {message: "Ya existe una categoria con este nombre."}
