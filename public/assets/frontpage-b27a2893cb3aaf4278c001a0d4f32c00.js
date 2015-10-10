ca99e378a869bc7088ee04d58d11ce4dcf1a4340
Hugo Gonzalez <hugo@Hugos-MacBook-Pro.local>
2015-10-10 16:34:38 -0500

    Changes to combo max price per unit, comment boxes now have adaptive sizes, and orders are displayed by most recent order.

---
+++ public/assets/frontpage-b27a2893cb3aaf4278c001a0d4f32c00.js
@@ -0,0 +1,44 @@
+
+var do_on_load = function() { 
+	console.log('first')
+	$('form#new_user').on('keypress', function(e) {
+			console.log('keypress')
+			if(e.which == 13) {
+		        $('form.new_user').submit();
+		    }
+	})
+	$(window).load(function(){
+		console.log('second')
+		$('.loading_screen').remove()
+		if ($('p.alert').text() == "") {
+			$('p.alert').css('display', "none")
+		};
+
+		$('#square3').on('click', function(e) {
+			console.log('click')
+			$('#square3 a')[0].click();
+		})
+
+		$('.frontpage-square a').on('click', function(e) {
+			e.stopImmediatePropagation();
+		})
+
+		$(".frontpage-square").on({
+		    mouseenter: function (e) {
+		        //stuff to do on mouse enter
+				$(e.target).parent().siblings('img').addClass('hover')
+		    },
+		    mouseleave: function (e) {
+		        //stuff to do on mouse leave
+				$(e.target).parent().siblings('img').removeClass('hover')
+		    }
+		});
+	})
+}
+
+
+
+
+$(document).ready(do_on_load)
+$(window).bind('page:change', do_on_load)
+;
